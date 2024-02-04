5c1ec05bb6f42c23034477ff18889fd142d29739
kimbapquyenduy <lequyenduy.tdc2020@gmail.com>
2024-02-04 23:05:53 +0700

    small update on popup

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -3,21 +3,24 @@ import { useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 
 export const Popup = ({ setIsOpen, isOpen, item }) => {
+  console.log(item);
   return (
     <>
       {isOpen ? (
         <div className="fixed w-[100%] h-[100vh] bg-black/80 justify-center items-center top-0 left-0 flex z-[999]">
-          <div className="relative w-[800px] h-[95vh] bg-black ">
+          <div className=" w-[800px] h-[95vh] bg-black ">
             <div className="w-full h-[60%] bg-white"></div>
             <div className="flex">
-              <div>
-                <div className="text-3xl text-white font-bold">
+              <div className="p-4 flex-[50%]">
+                <p className="text-3xl text-white font-bold">
                   {item?.title ? item?.title : item.name}
-                </div>
+                </p>
+                <p className="text-white">{item?.release_date}</p>
+                <p className="w-full  text-white break-words whitespace-pre-wrap">
+                  {item?.overview}
+                </p>
               </div>
-              <div>
-                <div className="text-white">Cate</div>
-              </div>
+              <div className="flex-[50%]"></div>
             </div>
             <div
               className="absolute top-2 right-2 text-xl rounded-full bg-black/80 text-white p-1 cursor-pointer z-[99999]"
