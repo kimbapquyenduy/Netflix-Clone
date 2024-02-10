c740d8b0a203d21e1c4a1db6dff0c18db9c5747d
kimbapquyenduy <lequyenduy.tdc2020@gmail.com>
2024-02-10 20:58:09 +0700

    Change some css and npm movie,youtube trailer,

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -13,26 +13,43 @@ export const Popup = ({ setIsOpen, isOpen, item }) => {
       setGenre(response.data.genres);
     });
   }, [requests.requestGenre]);
-  console.log(genre.map((item) => item));
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else return str;
+  };
+  console.log(item);
   return (
     <>
       {isOpen ? (
         <div className="fixed w-[100%] h-[100vh] bg-black/80 justify-center items-center top-0 left-0 flex z-[999]">
           <div className=" w-[1000px] h-[95vh] bg-black ">
-            <div className="w-full h-[60%] bg-white"></div>
-            <div className="flex">
-              <div className="p-4 flex-[70%]">
-                <p className="text-3xl text-white font-bold">
+            <div className="w-full h-[65%] bg-white relative">
+              <div
+                className="absolute top-2 right-2 text-xl rounded-full bg-black/80 text-white p-1 cursor-pointer z-[99999]"
+                onClick={() => setIsOpen(false)}
+              >
+                <AiOutlineClose />
+              </div>
+            </div>
+            <div className="flex ">
+              <div className="px-6 py-4 flex-[70%] ">
+                <p className="text-3xl text-white font-bold ">
                   {item?.title ? item?.title : item.name}
                 </p>
 
-                <p className="w-full  text-white break-words whitespace-pre-wrap">
-                  {item?.overview}
+                <p className="w-full text-white break-words whitespace-pre-wrap mt-2  ">
+                  {truncateString(item?.overview, 300)}
                 </p>
               </div>
-              <div className="flex-[30%]">
-                <p className="text-white">Release Date: {item?.release_date}</p>
+              <div className="mt-14 flex-[30%]">
                 <p className="text-white">
+                  Release Date:{" "}
+                  {item?.release_date
+                    ? item?.release_date
+                    : item?.first_air_date}
+                </p>
+                <p className="text-white mt-2">
                   Genre:{" "}
                   {item?.genre_ids.map((genlist) =>
                     genre
@@ -45,12 +62,6 @@ export const Popup = ({ setIsOpen, isOpen, item }) => {
                   )}
                 </p>
               </div>
-            </div>
-            <div
-              className="absolute top-2 right-2 text-xl rounded-full bg-black/80 text-white p-1 cursor-pointer z-[99999]"
-              onClick={() => setIsOpen(false)}
-            >
-              <AiOutlineClose />
             </div>
           </div>
         </div>
