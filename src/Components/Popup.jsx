1ada1550b57861d5db476d3a7f50a5e479ab30f7
kimbapquyenduy <lequyenduy.tdc2020@gmail.com>
2024-02-10 22:23:44 +0700

    Add trailer(ver 1 some of them have the wrong api)

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -2,12 +2,26 @@ import axios from "axios";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { AiOutlineClose } from "react-icons/ai";
+import { AiFillYoutube, AiOutlineClose } from "react-icons/ai";
 import requests from "../Requests";
-
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 export const Popup = ({ setIsOpen, isOpen, item }) => {
   const [genre, setGenre] = useState([]);
-
+  const [urlTrailer, setUrlTrailer] = useState([]);
+  movieTrailer(null, { tmdbId: item?.id })
+    .then((url) => {
+      const urlParams = new URLSearchParams(new URL(url).search);
+      setUrlTrailer(urlParams.get("v"));
+    })
+    .catch((error) => console.log(error));
+  const opts = {
+    height: "100%",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
   useEffect(() => {
     axios.get(requests.requestGenre).then((response) => {
       setGenre(response.data.genres);
@@ -23,16 +37,18 @@ export const Popup = ({ setIsOpen, isOpen, item }) => {
     <>
       {isOpen ? (
         <div className="fixed w-[100%] h-[100vh] bg-black/80 justify-center items-center top-0 left-0 flex z-[999]">
-          <div className=" w-[1000px] h-[95vh] bg-black ">
-            <div className="w-full h-[65%] bg-white relative">
+          <div className=" w-[1000px] h-[95vh] bg-zinc-800 ">
+            <div className="w-full h-[70%] bg-white relative">
               <div
                 className="absolute top-2 right-2 text-xl rounded-full bg-black/80 text-white p-1 cursor-pointer z-[99999]"
                 onClick={() => setIsOpen(false)}
               >
                 <AiOutlineClose />
               </div>
+              <div></div>
+              <YouTube videoId={urlTrailer} opts={opts} className="h-full" />
             </div>
-            <div className="flex ">
+            <div className="flex align-middle justify-center">
               <div className="px-6 py-4 flex-[70%] ">
                 <p className="text-3xl text-white font-bold ">
                   {item?.title ? item?.title : item.name}
@@ -49,7 +65,7 @@ export const Popup = ({ setIsOpen, isOpen, item }) => {
                     ? item?.release_date
                     : item?.first_air_date}
                 </p>
-                <p className="text-white mt-2">
+                <p className="text-white mt-1">
                   Genre:{" "}
                   {item?.genre_ids.map((genlist) =>
                     genre
