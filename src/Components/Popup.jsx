5450d536098ba114b90fa55a033f66835cac11e1
kimbapquyenduy <lequyenduy.tdc2020@gmail.com>
2024-02-12 23:01:50 +0700

    Some visual update on the popup

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -36,37 +36,42 @@ export const Popup = ({ setIsOpen, isOpen, item }) => {
   return (
     <>
       {isOpen ? (
-        <div className="fixed w-[100%] h-[100vh] bg-black/80 justify-center items-center top-0 left-0 flex z-[999]">
-          <div className=" w-[1000px] h-[95vh] bg-zinc-800 ">
-            <div className="w-full h-[70%] bg-white relative">
+        <div className="fixed w-[100%] h-[100vh] bg-black/90 justify-center items-center top-0 left-0 flex z-[999]">
+          <div className=" w-[800px] h-[97vh] bg-[#181818] ">
+            <div className="w-full h-[70%] bg-white relative ">
+              <div className="absolute bottom-0 w-full h-[35%] bg-gradient-to-t from-[#181818]"></div>
               <div
-                className="absolute top-2 right-2 text-xl rounded-full bg-black/80 text-white p-1 cursor-pointer z-[99999]"
+                className="absolute top-4 right-2 text-xl rounded-full bg-black/80 text-white p-1 cursor-pointer z-[99999]"
                 onClick={() => setIsOpen(false)}
               >
                 <AiOutlineClose />
               </div>
-              <div></div>
-              <YouTube videoId={urlTrailer} opts={opts} className="h-full" />
+
+              <YouTube
+                videoId={urlTrailer}
+                opts={opts}
+                className="h-full w-full "
+              />
             </div>
             <div className="flex align-middle justify-center">
-              <div className="px-6 py-4 flex-[70%] ">
+              <div className="px-6 pb-2 flex-[65%] ">
                 <p className="text-3xl text-white font-bold ">
                   {item?.title ? item?.title : item.name}
                 </p>
 
-                <p className="w-full text-white break-words whitespace-pre-wrap mt-2  ">
+                <p className="w-full text-white break-words whitespace-pre-wrap mt-5 text-sm ">
                   {truncateString(item?.overview, 300)}
                 </p>
               </div>
-              <div className="mt-14 flex-[30%]">
-                <p className="text-white">
-                  Release Date:{" "}
+              <div className="mt-6 flex-[35%]">
+                <p className="text-white text-sm">
+                  <span className="text-white/50"> Release Date:</span>{" "}
                   {item?.release_date
                     ? item?.release_date
                     : item?.first_air_date}
                 </p>
-                <p className="text-white mt-1">
-                  Genre:{" "}
+                <p className="text-white mt-2 text-sm">
+                  <span className="text-white/50"> Genre:</span>{" "}
                   {item?.genre_ids.map((genlist) =>
                     genre
                       .filter((obj) => {
@@ -76,6 +81,10 @@ export const Popup = ({ setIsOpen, isOpen, item }) => {
                         obj.name == "" ? obj.name : obj.name + ", "
                       )
                   )}
+                </p>
+                <p className="text-white mt-2 text-sm">
+                  <span className="text-white/50"> Vote Average:</span>{" "}
+                  {Math.round(item.vote_average * 10) / 10}/10
                 </p>
               </div>
             </div>
